bec24bfdec1c27f8a2e39c22183660a2c120c6f0
Urs Schönholzer <urs.schoenholzer@gmx.net>
2018-03-03 13:35:17 +0100

    version 2

--- Ferraris.py
+++ Ferraris.py
@@ -6,10 +6,9 @@ kwh = 0
 
 counter=0
 
-
 def zaehler():
     global counter
-    counter= counter+1
+    counter = counter+1
     print(counter,"x")
 
 
@@ -19,17 +18,16 @@ def auswerter():
     
 sensor = LineSensor(4)
 
-sensor.when_line = zaehler
-sensor.when_no_line = auswerter
 
-print(counter,"y")
+while True:
+    sensor.when_line = zaehler
+#sensor.when_no_line = auswerter
 
+    #print(counter,"y")
+
+
+"""
 if counter==12:
     print("0.1 KWh verbraucht!")
     kwh=+0.1
- 
-
-
-
-pause()
-
+""" 
